4d32ab4303b5beb2413d1bd5b82f4dc5f637f087
Thomas bauer <thomas.bauer@fem.tu-ilmenau.de>
2018-07-02 23:27:27 +0200

    added Aufgabe 3b with template std::exponential_distribution and double

--- src/LTS-Aufgabe3b.cxx
+++ src/LTS-Aufgabe3b.cxx
@@ -9,19 +9,21 @@ int main()
 {
 	int sim_steps = 0;
 	double throuput_old = 1.0;
+	double mean_jobs_old = 1.0;
 	Server myServer = Server();
-	Simulation<std::geometric_distribution<int>, unsigned int> sim(0.2, 0.5, myServer);
-	while ((throuput_old - sim.getThroughput())>0.0000001) {
+	Simulation<std::exponential_distribution<double>, double> sim(0.2, 0.5, myServer);
+	while (((throuput_old - sim.getThroughput())>0.0000001) || ((mean_jobs_old - sim.getMeanJobs()) > 0.0001)) {
 		throuput_old = sim.getThroughput();
+		mean_jobs_old = sim.getMeanJobs();
 		for (int i = 0; i<100; i++) {
 			sim.step();
 			sim_steps++;
 		}
 
 	}
-	std::cout<< "Durchsatz: " << sim.getThroughput()/sim_steps <<std::endl;
-	std::cout << "Mittlere Auftragsanzahl: " << sim.getMeanJobs()/sim_steps << std::endl;
+	std::cout << "Durchsatz: " << sim.getThroughput() / sim_steps << std::endl;
+	std::cout << "Mittlere Auftragsanzahl: " << sim.getMeanJobs() / sim_steps << std::endl;
 
-    return 0;
+	return 0;
 }
 
